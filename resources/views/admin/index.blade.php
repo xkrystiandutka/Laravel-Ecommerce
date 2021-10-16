ffe663c51949a9be48e4a81d377d87eb11b4b231
Krystian <Krystian.Dutkait@gmail.com>
2021-10-16 12:01:07 +0200

    Admin panel refactoring
    I'm using another blade.php page to split the admin panel code for more convenience and better code review.

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -129,9 +129,9 @@
                 </div>
             </div>
             <div class="col-xl-6 col-12">
-                <div class="box bg-info bg-img" style="background-image: url(../images/gallery/bg-1.png)">
+                <div class="box bg-info bg-img" style="background-image: url(asset('')backend{{ asset('backend/images/gallery/bg-1.png') }})">
                     <div class="box-body text-center">
-                        <img src="../images/trophy.png" class="mt-50" alt="" />
+                        <img src="{{ asset('backend/images/trophy.png') }}" class="mt-50" alt="" />
                         <div class="max-w-500 mx-auto">
                             <h2 class="text-white mb-20 font-weight-500">Best Employee Johen,</h2>
                             <p class="text-white-50 mb-10 font-size-20">You've got 50.5% more sales today. You've reached 8th milestone, checkout author section</p>
@@ -215,7 +215,7 @@
                                 </div>
                             </div>
                             <div class="col-md-6 col-12">
-                                <div class="box no-shadow mb-0 bg-img rounded-0" data-overlay="5" style="background-image: url(../images/gallery/landscape7.jpg)">
+                                <div class="box no-shadow mb-0 bg-img rounded-0" data-overlay="5" style="background-image: url(asset('backend/images/gallery/landscape7.jpg'))">
                                     <div class="box-header no-border">
                                         <h4 class="box-title mb-0">
                                             <span class="avatar avatar-lg bg-success">DK</span>
@@ -273,7 +273,7 @@
                                         <td class="pl-0 py-8">
                                             <div class="d-flex align-items-center">
                                                 <div class="flex-shrink-0 mr-20">
-                                                    <div class="bg-img h-50 w-50" style="background-image: url(../images/gallery/creative/img-1.jpg)"></div>
+                                                    <div class="bg-img h-50 w-50" style="background-image: url(asset('backend/images/gallery/creative/img-1.jpg'))"></div>
                                                 </div>
 
                                                 <div>
@@ -318,7 +318,7 @@
                                         <td class="pl-0 py-8">
                                             <div class="d-flex align-items-center">
                                                 <div class="flex-shrink-0 mr-20">
-                                                    <div class="bg-img h-50 w-50" style="background-image: url(../images/gallery/creative/img-2.jpg)"></div>
+                                                    <div class="bg-img h-50 w-50" style="background-image: url(asset('backend/images/gallery/creative/img-2.jpg'))"></div>
                                                 </div>
 
                                                 <div>
@@ -363,7 +363,7 @@
                                         <td class="pl-0 py-8">
                                             <div class="d-flex align-items-center">
                                                 <div class="flex-shrink-0 mr-20">
-                                                    <div class="bg-img h-50 w-50" style="background-image: url(../images/gallery/creative/img-3.jpg)"></div>
+                                                    <div class="bg-img h-50 w-50" style="background-image: url(asset('backend/images/gallery/creative/img-3.jpg'))"></div>
                                                 </div>
 
                                                 <div>
@@ -408,7 +408,7 @@
                                         <td class="pl-0 py-8">
                                             <div class="d-flex align-items-center">
                                                 <div class="flex-shrink-0 mr-20">
-                                                    <div class="bg-img h-50 w-50" style="background-image: url(../images/gallery/creative/img-4.jpg)"></div>
+                                                    <div class="bg-img h-50 w-50" style="background-image: url(asset('backend/images/gallery/creative/img-4.jpg'))"></div>
                                                 </div>
 
                                                 <div>
@@ -453,7 +453,7 @@
                                         <td class="pl-0 py-8">
                                             <div class="d-flex align-items-center">
                                                 <div class="flex-shrink-0 mr-20">
-                                                    <div class="bg-img h-50 w-50" style="background-image: url(../images/gallery/creative/img-5.jpg)"></div>
+                                                    <div class="bg-img h-50 w-50" style="background-image: url(asset('backend/images/gallery/creative/img-5.jpg'))"></div>
                                                 </div>
 
                                                 <div>
