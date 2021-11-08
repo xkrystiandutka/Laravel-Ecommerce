e8eb0de85ebeb97b26620b481fc0d11d06ade2b8
Krystian <Krystian.Dutkait@gmail.com>
2021-11-08 12:38:57 +0100

    Slider Active/Inactive

--- app/Http/Controllers/Backend/SliderController.php
+++ app/Http/Controllers/Backend/SliderController.php
@@ -111,4 +111,29 @@ class SliderController extends Controller
             return redirect()->back()->with($notification);
 
         } // end method
+        public function SliderInactive($id){
+            Slider::findOrFail($id)->update(['status' => 0]);
+
+            $notification = array(
+                'message' => 'Slider Inactive Successfully',
+                'alert-type' => 'info'
+            );
+
+            return redirect()->back()->with($notification);
+
+        } // end method
+
+
+        public function SliderActive($id){
+            Slider::findOrFail($id)->update(['status' => 1]);
+
+            $notification = array(
+                'message' => 'Slider Active Successfully',
+                'alert-type' => 'info'
+            );
+
+            return redirect()->back()->with($notification);
+
+        } // end method
+
 }
