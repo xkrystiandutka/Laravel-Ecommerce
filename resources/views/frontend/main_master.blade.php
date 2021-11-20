062e63074de9f99cf4f466ff5044012467561ef6
Krystian <Krystian.Dutkait@gmail.com>
2021-11-20 14:53:13 +0100

    Add ajax to find product id

--- resources/views/frontend/main_master.blade.php
+++ resources/views/frontend/main_master.blade.php
@@ -1,35 +1,3 @@
-Skip to content
-Search or jump to…
-Pull requests
-Issues
-Marketplace
-Explore
-
-@xkrystiandutka
-easylearningbd
-/
-advance-ecommerce-project
-Public
-8
-2227
-Code
-Issues
-3
-Pull requests
-2
-Actions
-Projects
-Wiki
-Security
-Insights
-advance-ecommerce-project/resources/views/frontend/main_master.blade.php
-@easylearningbd
-easylearningbd 102 Product Modal Design Part 1
-Latest commit 5b856fd on 27 Feb
- History
- 1 contributor
-171 lines (124 sloc)  5.91 KB
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -37,6 +5,7 @@ Latest commit 5b856fd on 27 Feb
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
 <meta name="description" content="">
+<meta name="csrf-token" content="{{ csrf_token() }}">
 <meta name="author" content="">
 <meta name="keywords" content="MediaCenter, Template, eCommerce">
 <meta name="robots" content="all">
@@ -61,21 +30,24 @@ Latest commit 5b856fd on 27 Feb
 <link href='http://fonts.googleapis.com/css?family=Roboto:300,400,500,700' rel='stylesheet' type='text/css'>
 <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,400italic,600,600italic,700,700italic,800' rel='stylesheet' type='text/css'>
 <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
-
-<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" >
-
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css">
 
 </head>
 <body class="cnt-home">
 <!-- ============================================== HEADER ============================================== -->
+
 @include('frontend.body.header')
 
 <!-- ============================================== HEADER : END ============================================== -->
+
 @yield('content')
+
 <!-- /#top-banner-and-menu -->
 
 <!-- ============================================================= FOOTER ============================================================= -->
+
 @include('frontend.body.footer')
+
 <!-- ============================================================= FOOTER : END============================================================= -->
 
 <!-- For demo purposes – can be removed on production -->
@@ -99,73 +71,78 @@ Latest commit 5b856fd on 27 Feb
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 
 <script>
- @if(Session::has('message'))
- var type = "{{ Session::get('alert-type','info') }}"
- switch(type){
-    case 'info':
-    toastr.info(" {{ Session::get('message') }} ");
-    break;
-    case 'success':
-    toastr.success(" {{ Session::get('message') }} ");
-    break;
-    case 'warning':
-    toastr.warning(" {{ Session::get('message') }} ");
-    break;
-    case 'error':
-    toastr.error(" {{ Session::get('message') }} ");
-    break;
- }
- @endif
+    @if (Session::has('message'))
+        var type = "{{ Session::get('alert-type','info') }}"
+        switch(type){
+            case 'info':
+                toastr.info("{{ Session::get('message') }}");
+            break;
+            case 'success':
+                toastr.success("{{ Session::get('message') }}");
+            break;
+            case 'warning':
+                toastr.warning("{{ Session::get('message') }}");
+            break;
+            case 'error':
+                toastr.error("{{ Session::get('message') }}");
+            break;
+            }
+    @endif
 </script>
-
-
 
 <!-- Add to Cart Product Modal -->
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Product Name </h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
 
-      <div class="modal-body">
+        <div class="modal-body">
 
-       <div class="row">
+         <div class="row">
 
-        <div class="col-md-4">
+          <div class="col-md-4">
 
-            <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
+              <div class="card" style="width: 18rem;">
 
-        </div><!-- // end col md -->
+                <img src=" " class="card-img-top" alt="..." style="height: 200px; width: 200px;">
+
+          </div><!-- // end col md -->
 
 
-        <div class="col-md-4">
+          <div class="col-md-4">
 
-     <ul class="list-group">
-  <li class="list-group-item">An item</li>
-  <li class="list-group-item">A second item</li>
-  <li class="list-group-item">A third item</li>
-  <li class="list-group-item">A fourth item</li>
-  <li class="list-group-item">And a fifth one</li>
-</ul>
+       <ul class="list-group">
+        <li class="list-group-item">Product Price: </li>
+        <li class="list-group-item">Product Code:</li>
+        <li class="list-group-item">Category:</li>
+        <li class="list-group-item">Brand:</li>
+        <li class="list-group-item">Stock</li>
+  </ul>
 
-        </div><!-- // end col md -->
+          </div><!-- // end col md -->
 
 
-        <div class="col-md-4">
+          <div class="col-md-4">
 
-            <div class="form-group">
-    <label for="exampleFormControlSelect1">Example select</label>
+              <div class="form-group">
+    <label for="exampleFormControlSelect1">Choose Color</label>
+      <select class="form-control" id="exampleFormControlSelect1">
+        <option>1</option>
+        <option>2</option>
+        <option>3</option>
+        <option>4</option>
+        <option>5</option>
+      </select>
+    </div>  <!-- // end form group -->
+
+
+    <div class="form-group">
+    <label for="exampleFormControlSelect1">Choose Size</label>
     <select class="form-control" id="exampleFormControlSelect1">
       <option>1</option>
       <option>2</option>
@@ -173,28 +150,48 @@ Latest commit 5b856fd on 27 Feb
       <option>4</option>
       <option>5</option>
     </select>
-  </div>
+  </div>  <!-- // end form group -->
 
-        </div><!-- // end col md -->
+       <div class="form-group">
+    <label for="exampleFormControlInput1">Quantity</label>
+    <input type="number" class="form-control" id="exampleFormControlInput1" value="1" min="1" >
+  </div> <!-- // end form group -->
 
+<button type="submit" class="btn btn-primary mb-2">Add to Cart</button>
 
-       </div> <!-- // end row -->
-
-
-
-
-
-
+          </div><!-- // end col md -->
 
 
+         </div> <!-- // end row -->
 
-
-      </div> <!-- // end modal Body -->
+        </div> <!-- // end modal Body -->
 
     </div>
   </div>
 </div>
 <!-- End Add to Cart Product Modal -->
+
+
+<script type="text/javascript">
+    $.ajaxSetup({
+        headers:{
+            'X-CSRF-TOKEN':$('meta[name="csrf-token"]').attr('content')
+        }
+    })
+// Start Product View with Modal
+function productView(id){
+    // alert(id)
+    $.ajax({
+        type: 'GET',
+        url: '/product/view/modal/'+id,
+        dataType:'json',
+        success:function(data){
+        }
+    })
+
+}
+</script>
+
 
 </body>
 </html>
