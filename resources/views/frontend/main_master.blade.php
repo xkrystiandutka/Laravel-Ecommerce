65a738f22b2da85fb7260e5563a1abd3c43af3f9
Krystian <Krystian.Dutkait@gmail.com>
2021-11-22 11:52:12 +0100

    Product add to cart with ajax

--- resources/views/frontend/main_master.blade.php
+++ resources/views/frontend/main_master.blade.php
@@ -96,7 +96,7 @@
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLabel"><strong><span id="pname"></span> </strong></h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close" id="closeModel">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
@@ -255,7 +255,7 @@ function productView(id){
             },
             url: "/cart/data/store/"+id,
             success:function(data){
-                console.log(data)
+                $('#closeModel').click();
             }
         })
     }
